e61fe41c99c9e90be858c92ab845f5978ac94dc2
kaanbayram <kaan.9898@hotmail.com>
2023-01-21 23:51:19 +0300

    [bugfix] - root file dest updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
-const connectDB = require('./config/db');
+const connectDB = require('./backend/config/db');
 const port = process.env.PORT || 4000;
 const cors = require('cors');
 
@@ -14,7 +14,7 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/products', require('./routes/productRoutes'));
+app.use('/api/products', require('./backend/routes/productRoutes'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
